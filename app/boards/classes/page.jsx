ab9ab85cfb2fe903b139df89dd3896a38d693c65
Jonathan Price <79269129+jonprice99@users.noreply.github.com>
2023-09-29 11:52:13 -0400

    Walking skeleton intro to Next.js

---
+++ app/boards/classes/page.jsx
@@ -0,0 +1,50 @@
+import Link from "next/link"
+import styles from './page.module.css'
+import Image from 'next/image'
+
+const classes = () => {
+    return (
+      <main className={styles.main}>
+      <div className={styles.description}>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a>
+          <h2>
+            Classes Board
+          </h2>
+          <p>(Construction in progress...)</p>
+        </a>
+      </div>
+    </main>)
+  }
+  
+export default classes
