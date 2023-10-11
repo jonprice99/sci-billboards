9010ec66d6fbbc93b1af56a319e362f03f6a5fd2
Jonathan Price <79269129+jonprice99@users.noreply.github.com>
2023-10-10 23:48:28 -0400

    Add rough functions to update home grid

--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
+'use client';
 import styles from './page.module.css'
+import { useState } from 'react';
 
 export default function Home() {
   // The set of pastel colors to be used as backgrounds for the link cards
@@ -14,86 +16,103 @@ export default function Home() {
   ];
 
   // The set of cards with their appropriate links and details
-  const cards = [
+  // To be replaced with a db table
+  const [cards, setCards] = useState([
     {
       href: '/boards/trending',
-      className: styles.card,
-      target: '_self',
-      rel: 'noopener noreferrer',
       header: '🔥 Trending',
       paragraph: 'See what ideas are hot and current here!',
     },
     {
       href: '/boards/career_services',
-      className: styles.card,
-      target: '_self',
-      rel: 'noopener noreferrer',
       header: '🧑‍💼 Career Services',
       paragraph: 'Tell your thoughts on our career resources!',
     },
     {
       href: '/boards/classes',
-      className: styles.card,
-      target: '_self',
-      rel: 'noopener noreferrer',
       header: '🧑‍💻 Classes',
       paragraph: 'Have feedback about current and existing classes? Give it here!',
     },
     {
       href: '/boards/classrooms',
-      className: styles.card,
-      target: '_self',
-      rel: 'noopener noreferrer',
       header: '🏫 Classrooms',
       paragraph: 'Is there room for improvement with our classrooms? Tell us here!',
     },
     {
       href: '/boards/community',
-      className: styles.card,
-      target: '_self',
-      rel: 'noopener noreferrer',
       header: '🌐 Community',
       paragraph: "Thoughts on SCI culture or social life? Let us know how you feel!",
     },
     {
       href: '/boards/curriculum',
-      className: styles.card,
-      target: '_self',
-      rel: 'noopener noreferrer',
       header: '📚 Curriculum ',
       paragraph:
         "Think our curriculum needs some updating? Let us know!",
     },
     {
       href: '/boards/events',
-      className: styles.card,
-      target: '_self',
-      rel: 'noopener noreferrer',
       header: '📅 Events ',
       paragraph:
         "Give us your thoughts on past events or ideas for new ones!",
     },
     {
       href: '/boards/lounges',
-      className: styles.card,
-      target: '_self',
-      rel: 'noopener noreferrer',
       header: '🛋️ Lounges ',
       paragraph:
         "Want better places to take it easy at SCI? Drop us some thoughts!",
     },
     {
       href: '/boards/misc',
-      className: styles.card,
-      target: '_self',
-      rel: 'noopener noreferrer',
       header: '🦆 Miscellaneous ',
       paragraph:
         "Got some random thoughts for SCI? We want them!",
     },
-  ]
+  ]);
+  
+  /**
+   * Note: To make any persistent changes to the card list, 
+   * we will need to store the 
+   */
 
-  // Function to get and cycle through each of the pastel colors
+  /**
+   * Function to add a new card to the grid
+   * @param {*} link The name of the board in the URL
+   * @param {*} title The name of the board in the Card
+   * @param {*} description The paragraph/description of the Card
+   */
+  function addCard({link, title, description}) {
+    setCards([
+      ...cards, 
+      {
+        href: `/boards/${link}`,
+        header: title,
+        paragraph: description,
+      },
+    ]);
+  }
+
+  /**
+   * Function to remove a card from the grid based on the title
+   * @param {*} header The title of the category you wish to remove
+   */
+  function removeCard(header) {
+    const index = cards.findIndex((card) => card.header === header);
+    if (index !== -1) {
+      const newCards = [...cards];
+      newCards.splice(index, 1);
+      setCards(newCards);
+    }
+  }
+
+  /**
+   * Note: We'll have to update addCard and removeCard once the db is
+   * up and running so we can also add and drop tables as needed
+   */
+
+  /**
+   * Function to get and cycle through each of the pastel colors
+   * @returns (String) Color code for the card
+   */
   function getNextPastelColor() {
     const color = pastelColors.shift();
     pastelColors.push(color);
@@ -108,10 +127,10 @@ export default function Home() {
           <a
             key={index}
             href={card.href}
-            className={card.className}
+            className={styles.card}
             style={{ backgroundColor: pastelColors[index % pastelColors.length] }}
-            target={card.target}
-            rel={card.rel}
+            target='_self'
+            rel='noopener noreferrer'
           >
             <h2>{card.header}</h2>
             <p>{card.paragraph}</p>
