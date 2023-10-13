9cb1150e00ad47b1579d3911a911860171d9cc14
Brian Kim <brk99@pitt.edu>
2023-10-13 13:20:44 -0400

    create user_post endpoint views.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import date
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+@api_view(['POST'])
+def user_post(request):
+    return
+
