6ae336c72be19adfa5a4703e0650ce6d0c9e2997
Jonathan Price <79269129+jonprice99@users.noreply.github.com>
2023-11-01 11:28:41 -0400

    Re-add Upvotes serializer

--- billboard/BillboardAPI/serializers.py
+++ billboard/BillboardAPI/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from .models import Categories, Posts
+from .models import *
 
 class CategoriesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categories
+        fields = '__all__'
+        
+class User_UpvotesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User_Upvotes
         fields = '__all__'
         
 class PostsSerializer(serializers.ModelSerializer):
