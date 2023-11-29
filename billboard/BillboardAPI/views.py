197856e49fab1edace940ca0bb0ee64da9ff94d8
Brian Kim <brk99@pitt.edu>
2023-11-29 13:44:00 -0500

    update views.py

--- billboard/BillboardAPI/views.py
+++ billboard/BillboardAPI/views.py
@@ -5,6 +5,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models import F
+
 import json
 
 from django.contrib.auth import login
