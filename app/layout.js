0306e9e8011127ef13a1e6a5a3a50d900fea96c5
Jonathan Price <79269129+jonprice99@users.noreply.github.com>
2023-11-29 18:30:57 -0500

    Fonts stored locally within app

--- app/layout.js
+++ app/layout.js
@@ -6,15 +6,26 @@
 import './globals.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import localFont from 'next/font/local'
 import { Inter } from 'next/font/google'  // Default Next font
 import { Rubik } from 'next/font/google'  // Primary Pitt font
-import { Open_Sans } from 'next/font/google'  // Secondary Pitt font
+//import { Open_Sans } from 'next/font/google'  // Secondary Pitt font
 import { Merriweather } from 'next/font/google' // Tertiary Pitt font
+
+// Pull the local font files
+const open_sans = localFont({
+  src: './OpenSans-VariableFont_wdth,wght.ttf',
+  display: 'swap',
+})
+const rubik = localFont({
+  src: './Rubik-VariableFont_wght.ttf',
+  display: 'swap',
+})
 
 // Hold each of the fonts
 const inter = Inter({ subsets: ['latin'] })
-const rubik = Rubik({ subsets: ['latin'] })
-const open_sans = Open_Sans({ subsets: ['latin'] })
+//const rubik = Rubik({ subsets: ['latin'] })
+//const open_sans = Open_Sans({ subsets: ['latin'] })
 const merriweather = Merriweather({ subsets: ['latin'], weight: ['400'] })
 
 export const metadata = {
